4be95dffd147d51626e4dd7003cdd6bba4c0965e
shobhnakailash12 <60381049+shobhnakailash12@users.noreply.github.com>
2020-01-29 06:40:44 -0600

    pretty

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -9,7 +9,7 @@ import cucumber.api.junit.Cucumber;
 	@RunWith(Cucumber.class)
 	
 	@CucumberOptions(features="FeatureFile",
-	glue= {"stepDefinition"},tags="@search",plugin = {"preety","json:target/cucumber.json"}
+	glue= {"stepDefinition"},tags="@search",plugin = {"pretty","json:target/cucumber.json"}
 			)
 	public class TestRunner {
 
